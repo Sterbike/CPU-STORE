bf9cb5c5016a0c3d6ac918d52848794b42fbee4f
adamcsanyi16 <csanyiadam8@gmail.com>
2023-04-26 14:00:00 +0200

    elso valtozat

---
+++ server.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3500;
+
+app.get("/", (req, res) => {
+  try {
+    res.status(200).json({ msg: "Sikeres elindítás!" });
+  } catch (error) {
+    res.status(500).json({ msg: "Valami elromlott!" });
+  }
+});
+
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
